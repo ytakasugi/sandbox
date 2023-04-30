ef9c5a9b78b7ee5ec91f29270ca706db07028ad1
ytakasugi <sh7.tibi0129@gmail.com>
2023-05-01 00:29:37 +0900

    add new vue project

--- vue/chapter03/computed/src/App.vue
+++ vue/chapter03/computed/src/App.vue
@@ -1,47 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from "vue";
+
+// 半径の初期値を乱数で取得
+const radiusInit = Math.round(Math.random() * 10);
+// 円周率のテンプレート変数を用意
+const PI = ref(3.14);
+// 半径のテンプレート変数を用意
+const radius = ref(radiusInit);
+// 円の面積の算出プロパティ
+const area = computed(
+  (): number => {
+    return radius.value * radius.value * PI.value;
+  }
+);
+
+// 半径のテンプレート変数に新しい乱数を1秒ごとに格納。
+setInterval(
+  (): void => {
+    radius.value = Math.round(Math.random() * 10);
+  },
+  1000
+)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>半径{{radius}}の円の面積を円周率{{PI}}で計算すると、{{area}}</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
