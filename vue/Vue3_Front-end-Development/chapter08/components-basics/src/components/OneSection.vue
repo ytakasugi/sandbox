1823a9b44ec34319681c1f76b8d3fa18a2f7de74
ytakasugi <sh7.tibi0129@gmail.com>
2023-05-02 16:43:43 +0900

    change files and add files

---
+++ vue/Vue3_Front-end-Development/chapter08/components-basics/src/components/OneSection.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="box">
+      <h4>ひとつのコンポーネント</h4>
+      <p>
+        コンポーネントとは、…
+      </p>
+    </section>
+  </template>
+  
+  <style scoped>
+  .box {
+    border: green 1px dashed;
+    margin: 10px;
+  }
+  </style>
+  
