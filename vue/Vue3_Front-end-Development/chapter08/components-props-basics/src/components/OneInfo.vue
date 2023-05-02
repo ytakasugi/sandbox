44a561788a6a76ae5c44887b4ce7c152d1bac86e
ytakasugi <sh7.tibi0129@gmail.com>
2023-05-03 00:32:44 +0900

    change files and add files

---
+++ vue/Vue3_Front-end-Development/chapter08/components-props-basics/src/components/OneInfo.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+interface Props {
+    title: string;
+    content: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <section class="box">
+        <h4>{{ title }}</h4>
+        <h4>{{ content }}</h4>
+    </section>
+</template>
+
+<style scoped>
+.box {
+    border: green 1px dashed;
+    margin: 10px;
+}
+</style>
